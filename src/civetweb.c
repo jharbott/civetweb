a9caba12ec129d74a78721a1b677b832d32e5622
feneuilflo <florent.feneuil@laposte.net>
2016-11-18 14:14:36 +0100

    remove leading whitespaces when matching websocket protocols

--- src/civetweb.c
+++ src/civetweb.c
@@ -9866,7 +9866,8 @@ handle_websocket_request(struct mg_connection *conn,
 				sep = strchr(protocol, ',');
 				curSubProtocol = protocol;
 				len = sep ? (unsigned long)(sep - protocol) : strlen(protocol);
-				protocol = sep ? sep + 1 : NULL;
+				while(sep && isspace(*++sep)); // ignore leading whitespaces
+				protocol = sep;
 
 
 				for (idx = 0; idx < subprotocols->nb_subprotocols; idx++) {
@@ -9901,7 +9902,8 @@ handle_websocket_request(struct mg_connection *conn,
 				 * and use it to select one protocol among those the client has
 				 * offered.
 				 */
-				conn->request_info.acceptedWebSocketSubprotocol = (sep + 1);
+				while(isspace(*++sep)); // ignore leading whitespaces
+				conn->request_info.acceptedWebSocketSubprotocol = sep;
 			}
 		}
 
